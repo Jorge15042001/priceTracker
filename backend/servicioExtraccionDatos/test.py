c53e0b002d4410bfceed4d586b01973d8b07c9f1
Jorge15042001 <adrivulgarin@hotmail.com>
2022-01-22 23:35:27 -0500

    not relevant

--- backend/servicioExtraccionDatos/test.py
+++ backend/servicioExtraccionDatos/test.py
@@ -1,4 +1,4 @@
 import requests
-res = requests.post('http://localhost:5000/', json={"url":"https://www.amazon.com/LG-32GN650-B-Ultragear-Reduction-FreeSync/dp/B08LLF9NS1/ref=psdc_1292115011_t4_B07YGZRQ98"})
+res = requests.post('http://localhost:5000/', json={"url":"https://articulo.mercadolibre.com.ec/MEC-506919172-apple-ipad-pro-11-128gb-wifi-chip-m1-_JM?searchVariation=174101054149#searchVariation=174101054149&position=4&search_layout=grid&type=item&tracking_id=105f8369-73e0-4076-ba01-c8d78b96e1c8"})
 if res.ok:
     print(res.json())
